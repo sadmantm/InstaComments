a9513646d2cd17edaa282aa3eea71407017b0f6f
SadMan <teteusg109@gmail.com>
2026-05-14 13:39:02 -0300

    teste4

--- comments.js
+++ comments.js
@@ -138,7 +138,8 @@ async function openCommentsTab(page, maxRetries = 3) {
 
     } catch (err) {
       console.warn(`[tab] ⚠️ Tentativa ${attempt} falhou: ${err.message}`);
-
+      const ss = path.resolve(`./cu_${profile}_${Date.now()}.png`);
+      await page.screenshot({ path: ss, fullPage: true });
       if (attempt < maxRetries) {
         console.log('[tab] Recarregando página antes de tentar novamente...');
         try {
